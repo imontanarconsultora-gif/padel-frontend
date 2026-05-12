db9c8fd200ed90e6707624bf1c497f372c865189
IVANAMONTANAR\ivana <ivanaxp@gmail.com>
2026-05-12 16:25:09 -0300

    se agrega la vista de cancelar

--- Pages/Cancelar.cshtml.cs
+++ Pages/Cancelar.cshtml.cs
@@ -2,8 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PadelWeb.Pages;
 
-public class CancelarModel(IConfiguration config) : PageModel
+public class CancelarModel : PageModel
 {
-    public string ApiUrl { get; private set; } = string.Empty;
-    public void OnGet() => ApiUrl = config["ApiUrl"] ?? "http://localhost:5000";
+    public void OnGet() { }
 }
